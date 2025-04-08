ceca0f9416feb564f2de765ce4c20b556786461e
Mohamed Shaffaq <mohamedshaffaq@gmail.com>
2025-04-08 15:51:41 +0530

    Added KeyHandler class
    created KeyHandler class and a function to draw a rectangle on the
    screen

--- My2Dgame/src/Default/GamePanel.java
+++ My2Dgame/src/Default/GamePanel.java
@@ -2,10 +2,12 @@ package Default;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel{ //panel is what is inside the border 
+public class GamePanel extends JPanel implements Runnable{ //panel is what is inside the border 
 	final int originalTileSize = 16; // 16x16 tile
 	final int scale = 3; // scaling the 16x16 character by 3x
 	
@@ -20,4 +22,64 @@ public class GamePanel extends JPanel{ //panel is what is inside the border
 		this.setBackground(Color.black);
 		this.setDoubleBuffered(true);
 	}
+
+	Thread gameThread;
+	
+	
+	public void startgameThread() {
+		gameThread = new Thread(this);
+		gameThread.start();		
+	}
+	
+	@Override
+	public void run() {
+		// Thread implemented to keep the game "running" meaning implement existence of time 
+		while(gameThread != null) {
+			System.out.println("Game thread is running");
+			
+			//1 Update: update information such as character positions
+			//2 Draw: draw the screen with the updated information
+			
+			update();
+			repaint();
+		}
+	}
+	
+	public void update() {
+		
+	}
+	
+	//function that draws the characters etc on the screen, it uses java graphics glass to draw it
+	public void paintComponent(Graphics g) { 
+		super.paintComponent(g);
+		
+		//Changing graphics class to graphics 2D class to get more functionality
+		Graphics2D g2 = (Graphics2D)g;
+		
+		g2.setColor(Color.white);
+		g2.fillRect(200, 200, tileSize, tileSize);
+		g2.dispose();
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
